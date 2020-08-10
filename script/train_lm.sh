3ddbca8f939cb501a3e89885ba38afbca1adf44f
Stanwang1210 <ch995308@gmail.com>
2020-08-10 17:08:17 +0800

    'upload'

--- script/train_lm.sh
+++ script/train_lm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CONFIG="librispeech_lm"
-DIR="/data/storage/harry/E2E_ASR"
+DIR="/work/b07u1234/b07701209/model_dir"
 
 echo "Start running training process of RNNLM"
 CUDA_VISIBLE_DEVICES=$2 python3 main.py --config config/${CONFIG}.yaml \
