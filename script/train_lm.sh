fc3b1ac26040a143a93dbf7a8792f849bccb1143
Stanwang1210 <ch995308@gmail.com>
2020-08-19 14:22:47 +0800

    'upload'

--- script/train_lm.sh
+++ script/train_lm.sh
@@ -6,7 +6,7 @@ DIR="/work/b07u1234/b07701209/model_dir"
 echo "Start running training process of RNNLM"
 CUDA_VISIBLE_DEVICES=$2 python3 main.py --config config/${CONFIG}.yaml \
     --name $1 \
-    --njobs 8 \
+    --njobs 16 \
     --seed 0 \
     --lm \
     --logdir ${DIR}/log_lm/ \
